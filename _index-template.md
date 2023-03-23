d4d6d12711fbaee2ccf16b8512bb5a4cbd609b3f
Elio Campitelli <eliocampitelli@gmail.com>
2023-03-22 23:08:38 -0300

    Automatiza crear colecciones

---
+++ _index-template.md
@@ -0,0 +1,12 @@
+---
+output: hugodown::md_document
+title: "Title"
+date: ${today}
+summary: ''
+trailer: "gallery/${folder}/splash.png"
+splash:
+  caption: "caption"
+---
+
+{{< foldergallery src="gallery/${folder}/imgs" >}}
+
---
+++ script.sh
@@ -0,0 +1,31 @@
+
+thumbifiy () {
+    export folder=$1
+    mogrify -path content/gallery/$folder/thumbs/ -thumbnail 512x512 content/gallery/$folder/imgs/*.png
+
+}
+gallery () {
+    export folder=$1
+    export imgfolder=$2
+    export today=$(date '+%Y-%m-%d')
+
+    if [ -d "content/gallery/$folder" ]; then
+        echo "La carpeta $folder ya existe!" 1>&2
+        return 1
+    fi
+
+    # Create gallery folder
+    mkdir content/gallery/$folder
+
+    # Create link to image folder
+    ln -s "$imgfolder" content/gallery/$folder/imgs
+
+    ## Create thumbnails
+    mkdir content/gallery/$folder/thumbs
+    mogrify -path content/gallery/$folder/thumbs/ -thumbnail 512x512 content/gallery/$folder/imgs/*.png
+
+    # Create _index.md from template
+    envsubst < _index-template.md > content/gallery/$folder/_index.md
+
+    xdg-open content/gallery/$folder
+}
